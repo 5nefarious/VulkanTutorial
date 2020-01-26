62b9635d2f0b8ff4159e32691a461c0a1279e97f
Arvin Ignaci <arvin.ignaci@gmail.com>
2020-01-26 16:25:32 -0500

    Created command buffers.

--- hello.cpp
+++ hello.cpp
@@ -82,6 +82,7 @@ private:
     std::vector<VkFramebuffer> swapChainFramebuffers;
     
     VkCommandPool commandPool;
+    std::vector<VkCommandBuffer> commandBuffers;
     
     void initWindow() {
         glfwInit();
@@ -103,6 +104,7 @@ private:
         createGraphicsPipeline();
         createFramebuffers();
         createCommandPool();
+        createCommandBuffers();
     }
     
     void mainLoop() {
@@ -486,6 +488,52 @@ private:
         VkResult result = vkCreateCommandPool(device, &poolInfo, nullptr, &commandPool);
         if (result != VK_SUCCESS)
             throw std::runtime_error("failed to create command pool!");
+    }
+    
+    void createCommandBuffers() {
+        commandBuffers.resize(swapChainFramebuffers.size());
+        
+        VkCommandBufferAllocateInfo allocInfo = {};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.commandPool = commandPool;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        allocInfo.commandBufferCount = (uint32_t) commandBuffers.size();
+        
+        VkResult result = vkAllocateCommandBuffers(device, &allocInfo, commandBuffers.data());
+        if (result != VK_SUCCESS)
+            throw std::runtime_error("failed to allocate command buffers!");
+        
+        for (size_t i = 0; i < commandBuffers.size(); i++) {
+            VkCommandBufferBeginInfo beginInfo = {};
+            beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+            
+            result = vkBeginCommandBuffer(commandBuffers[i], &beginInfo);
+            if (result != VK_SUCCESS)
+                throw std::runtime_error("failed to begin recording command buffer!");
+                
+            VkRenderPassBeginInfo renderPassInfo = {};
+            renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+            renderPassInfo.renderPass = renderPass;
+            renderPassInfo.framebuffer = swapChainFramebuffers[i];
+            
+            renderPassInfo.renderArea.offset = {0, 0};
+            renderPassInfo.renderArea.extent = swapChainExtent;
+            
+            VkClearValue clearColor = {0.0f, 0.0f, 0.0f, 1.0f};
+            renderPassInfo.clearValueCount = 1;
+            renderPassInfo.pClearValues = &clearColor;
+            
+            vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+            vkCmdBindPipeline(commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
+            
+            vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+            
+            vkCmdEndRenderPass(commandBuffers[i]);
+            
+            result = vkEndCommandBuffer(commandBuffers[i]);
+            if (result != VK_SUCCESS)
+                throw std::runtime_error("failed to record command buffer!");
+        }
     }
     
     bool checkValidationLayerSupport() {
